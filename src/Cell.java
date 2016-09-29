bb0a2fa44ff107e6a689077a1f69e1111c4171a1
Akhil Reddy <kakhilreddy1996@gmail.com>
2016-09-29 08:31:44 +0530

    Update Cell.java

--- src/Cell.java
+++ src/Cell.java
@@ -1,13 +1,14 @@
 /**
- * Created by ASUS on 9/25/2016.
+ * Created by AKHIL on 9/25/2016.
  */
+//This class represents each cell present on the board
 public class Cell {
     int row;
     int col;
     boolean isMine;
     boolean isOpen;
     boolean isFlag;
-    int mineCount;
+    int mineCount; //neighbour mines count
     Cell(int row,int col,boolean isMine)
     {
         this.col=col;
